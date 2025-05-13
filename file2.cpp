9bd499d3536d647f722def0a9602766b68d58b11
rixkifadil@gmail.com <rixkifadil@gmail.com>
2025-05-13 18:55:13 +0700

    add parameter construstor

--- file2.cpp
+++ file2.cpp
@@ -8,4 +8,9 @@ class mahasiswa {
         mahasiswa(int nim, string nama);
 };
 
-
+//definisi constuctor dengan parameter
+mahasiswa::mahasiswa(int nim, string nama) {
+    cout << "Constructor dengan Parameter Terpanggil" << endl;
+    cout << "NIM : " << nim << endl;
+    cout << "Nama : " << nama << endl;
+}
