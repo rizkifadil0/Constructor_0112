452527144a9922f10f4e3cde91e016809fba3096
rixkifadil@gmail.com <rixkifadil@gmail.com>
2025-05-13 19:18:31 +0700

    add int main

--- file3.cpp
+++ file3.cpp
@@ -10,7 +10,7 @@ class mahasiswa{
     mahasiswa();
     mahasiswa(int);
     mahasiswa(string);
-    mahasiswa(int inim, string iNama );
+    mahasiswa(int iNim, string iNama );
     void cetak();
 };
 
@@ -24,4 +24,27 @@ mahasiswa :: mahasiswa(int iNim){
 
 mahasiswa :: mahasiswa(string iNama){
     nama = iNama; // definisi hanya NAMA
+}
+
+mahasiswa :: mahasiswa(int iNim, string iNama){
+    nim = iNim; // definisi NIM dan NAMA
+    nama = iNama;
+}
+
+void mahasiswa::cetak(){
+    cout << endl << "Nim = " << nim << endl;
+    cout << "Nama = " << nama << endl;
+}
+
+int main(){
+    mahasiswa mhs1(102030); //hanya nim
+    mahasiswa mhs2("Andi"); //hanya nama
+    mahasiswa mhs3(010203, "Santi");    //nim dan nama
+
+    //tampilkan nilai
+    cout << "Mahasiswa 1: "; mhs1.cetak();
+    cout << "Mahasiswa 2: "; mhs2.cetak();
+    cout << "Mahasiswa 3: "; mhs3.cetak();
+
+    return 0;
 }
