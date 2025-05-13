54beb36d59085912dfde2a930511eb3780d70c00
rixkifadil@gmail.com <rixkifadil@gmail.com>
2025-05-13 19:07:08 +0700

    add constructor

--- file3.cpp
+++ file3.cpp
@@ -13,3 +13,15 @@ class mahasiswa{
     mahasiswa(int inim, string iNama );
     void cetak();
 };
+
+mahasiswa :: mahasiswa(){
+
+}
+
+mahasiswa :: mahasiswa(int iNim){
+    nim = iNim; // definisi hanya NIM
+}
+
+mahasiswa :: mahasiswa(string iNama){
+    nama = iNama; // definisi hanya NAMA
+}
